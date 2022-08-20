d35d60390e46dfe957c13accf56353b45f4920f2
Djalma Henrique <contato@djalmahenry.com>
2022-08-19 23:05:04 -0300

    feat: add project base

---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: "#131016",
+        alignItems: "center",
+        padding: 24
+    },
+
+    text: {
+        fontSize: 24,
+        fontWeight: "bold",
+        color: "#FDFCFE",
+        marginTop: 48
+    },
+});
